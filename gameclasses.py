ea61ca6ee651402ef660148bf59abfc7f5458fee
Dawdre <edward.scull11@gmail.com>
2018-06-12 00:15:23 +0100

    game class

--- gameclasses.py
+++ gameclasses.py
@@ -5,3 +5,16 @@ class Game:
     @property
     def noOfQuestions(self):
         return self._noOfQuestions
+
+    @noOfQuestions.setter
+    def noOfQuestions(self, value):
+        if value < 1:
+            self._noOfQuestions = 1
+            print("Minimum Number of Questions = 1")
+            print("Hence, number of questions will be set to 1")
+        else if value > 10:
+            self._noOfQuestions = 10
+            print("Maximum Number of Questions = 10")
+            print("Hence, number of questions will be set to 10")
+        else:
+            self._noOfQuestions = value
